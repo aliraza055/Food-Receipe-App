b87168ea899bdf430758404d3b1d0c7794e40e60
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-08-24 20:56:18 +0500

    change the background color of container

--- lib/views/explore_page.dart
+++ lib/views/explore_page.dart
@@ -15,38 +15,38 @@ class _ExplorePageState extends State<ExplorePage> {
       body: Column(
         children: [
           SizedBox(height: 50,),
-          Expanded(
-            child: StreamBuilder(
-              stream: FirebaseFirestore.instance.collection("categories").snapshots(), 
-              builder: (context,snapshot){
-                if(snapshot.hasError){
-                  print("you have an error");
-                } else if(snapshot.hasData ){
-                  final item=snapshot.data!.docs;
-                  return ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: item.length,
-                    itemBuilder: (context,index){
-                    return  Container(
-                      margin: EdgeInsets.all(12),
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Column(
-                        children: [
-                          Image.network(item[index]['image'],height: 50,width: 50,),
-                                                    Text(item[index]['name']),
-
-                        ],
-                      ),);
-                            
-                    });
-                   
-                }else{
-             print('hlee');
-                }
-                return Center(child: Text("Empty"));
-              } 
-              ),
+          SizedBox(
+            height: 120,
+            child: Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance.collection("categories").snapshots(), 
+                builder: (context,snapshot){
+                  if(snapshot.hasError){
+                    print("you have an error");
+                  } else if(snapshot.hasData ){
+                    final item=snapshot.data!.docs;
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: item.length,
+                      itemBuilder: (context,index){
+                      return  Container(
+                        height: 50,
+                        margin: EdgeInsets.all(12),
+                        padding: EdgeInsets.all(10),
+                        color: Colors.transparent,
+                        child: Column(
+                          children: [
+                            Image.network(item[index]['image'],height: 50,width: 50,),
+                           Text(item[index]['name']),
+                          ],
+                        ),);
+                              
+                      });                     
+                  }
+                  return Center(child: Text("Empty"));
+                } 
+                ),
+            ),
           )
         ],
       ),
