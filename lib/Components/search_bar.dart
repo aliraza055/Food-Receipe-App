b1bca0e374ed3cd38fec4d20d20fabe0a9022e7d
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-08-31 21:49:34 +0500

    change categori icon

--- lib/Components/search_bar.dart
+++ lib/Components/search_bar.dart
@@ -5,36 +5,43 @@ class SearchBa extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.symmetric(horizontal: 27),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 27),
       child: Row(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(30),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 7,
-                  color: Colors.black38
-                )
-              ]
-            ),
-            child:Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(),
-            ) ,
-          ),
-          SizedBox(width: 8,),
-          Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black38
+          // Wrap with Expanded so it takes available space
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 7,
+                    color: Colors.black38,
+                  ),
+                ],
               ),
-            shape: BoxShape.circle,
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Search...",
+                  ),
+                ),
+              ),
             ),
-            child: Icon(Icons.tune),
-          )
+          ),
+          const SizedBox(width: 8),
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black38),
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(Icons.tune),
+          ),
         ],
       ),
     );
--- lib/views/explore_page.dart
+++ lib/views/explore_page.dart
@@ -10,6 +10,8 @@ class ExplorePage extends StatefulWidget {
 }
 
 class _ExplorePageState extends State<ExplorePage> {
+  int selectedIndex = 0; // track selected category
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,37 +19,68 @@ class _ExplorePageState extends State<ExplorePage> {
         child: Padding(
           padding: const EdgeInsets.all(12),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SearchBa(),
-             // SizedBox(height: 50,),
-              SizedBox(
-                height: 120,
-                child: StreamBuilder(
-                  stream: FirebaseFirestore.instance.collection("categories").snapshots(), 
-                  builder: (context,snapshot){
-                    if(snapshot.hasData ){
-                      final item=snapshot.data!.docs;
-                      return ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: item.length,
-                        itemBuilder: (context,index){
-                        return  Container(
-                          margin: EdgeInsets.all(12),
-                          padding: EdgeInsets.all(10),
-                          color: Colors.transparent,
-                          child: Column(
-                            children: [
-                             Image.network(item[index]['image'],height: 50,width: 50,),
-                             Text(item[index]['name']),
-                            ],
-                          ),);
-                                
-                        });                     
-                    }
-                    return Center(child: Text("Empty"));
-                  } 
-                  ),
+              const SearchBa(),
+              const SizedBox(height: 20),
+            
+              StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("categories")
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final item = snapshot.data!.docs;
+                     return SizedBox(
+  height: 90,
+  child: ListView.builder(
+    scrollDirection: Axis.horizontal,
+    itemCount: item.length,
+    itemBuilder: (context, index) {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+      
+        child: Container(
+          color: Colors.amber,
+          margin: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            children: [
+             Image.network(item[index]['image'],height: 50,width: 50,),
+              
+
+              const SizedBox(height: 6),
+
+              Text(
+                item[index]['name'],
+                style: TextStyle(
+                  fontWeight:
+                      selectedIndex== index ? FontWeight.bold : FontWeight.normal,
+                  color: selectedIndex==index ? Colors.black : Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 10,),
+           
+              Container(
+                height: 3,
+                width: 50,
+                color: selectedIndex==index ? Colors.black :Colors.transparent,
               )
+            ],
+          ),
+        ),
+      );
+    },
+  ),
+);
+
+                  }
+                  return const Center(child: Text("Empty"));
+                },
+              ),
             ],
           ),
         ),
